fafe1e9e26bcdbd5be23978f2533cb4dbc87f17d
lizzieb1416 <lisetbastidasg@gmail.com>
2022-01-11 20:27:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      recipes: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('data.json')
+      .then(response => response.json())
+      .then(data => this.setState({ recipes: data }))
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  render() {
+    const { recipes, searchField } = this.state;
+    const filteredRecipes = recipes.filter(recipe => 
+      recipe.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      )
+    return (
+      <div className="App">
+        <h1>Recipes</h1>
+        <SearchBox
+          placeholder='search recipe'
+          handleChange={this.handleChange}
+        />
+        <CardList recipes={filteredRecipes}/>
+      </div>
+    )
+  }
+
 }
 
 export default App;
